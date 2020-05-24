31ca4089121b765289263dda1a70f65d9c228e5d
rodgersj097 <200320013@student.georgianc.on.ca>
2020-05-24 17:50:36 -0400

    First layout draft

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+/* GET home page. */
+router.get('/index2', function(req, res, next) {
+  res.render('index2');
+});
+
+
+module.exports = router;
